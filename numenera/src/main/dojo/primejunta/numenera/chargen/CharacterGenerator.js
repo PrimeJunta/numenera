582c47b00b7b2157edeff9d1627efd7cafb84a7e
psulonen <petteri.sulonen@iki.fi>
2013-09-18 09:47:54 +0300

    Updates version numbers and adds license info.

--- numenera/src/main/dojo/primejunta/numenera/chargen/CharacterGenerator.js
+++ numenera/src/main/dojo/primejunta/numenera/chargen/CharacterGenerator.js
@@ -93,7 +93,7 @@ function( declare,
         /**
          * Public version number.
          */
-        version : "1.3.1",
+        version : "1.3.2",
         /**
          * Set when a character is first advanced past creation.
          */
