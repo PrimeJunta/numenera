1734f5a5d350b0d6a8b1fb9c6bf0622d037bef45
psulonen <petteri.sulonen@iki.fi>
2013-09-21 16:39:43 +0300

    Our animation is playing as intended.

--- numenera/src/main/dojo/primejunta/numenera/cyphergen/CypherGenerator.js
+++ numenera/src/main/dojo/primejunta/numenera/cyphergen/CypherGenerator.js
@@ -2,6 +2,9 @@ define([ "dojo/_base/declare",
          "dojo/_base/lang",
          "dojo/query",
          "../_startup",
+         "dojo/_base/fx",
+         "dojox/fx/flip",
+         "dojo/on",
          "dijit/_WidgetBase",
          "dijit/_TemplatedMixin",
          "dijit/_WidgetsInTemplateMixin",
@@ -12,6 +15,9 @@ function( declare,
           lang,
           domQuery,
           _startup,
+          baseFx,
+          flip,
+          on,
           _WidgetBase,
           _TemplatedMixin,
           _WidgetsInTemplateMixin,
@@ -32,12 +38,35 @@ function( declare,
         {
             this.start(); // from startup
         },
+        showCypher : function()
+        {
+            this._flip( this.cypherCardBack, this.cypherCardFront, "white" );
+        },
+        hideCypher : function()
+        {
+            this._flip( this.cypherCardFront, this.cypherCardBack, "black" );
+        },
         /**
          * Calls _showHelp with about (that's an included text module).
          */
         showHelp : function()
         {
             this._showHelp( about );
+        },
+        _flip : function( from, to, endColor )
+        {
+            var anim = flip.flip({ 
+                node: from,
+                dir: "right",
+                depth: .3,
+                duration:400,
+                endColor : endColor
+            });
+            anim.onEnd = lang.hitch( this, function(){ 
+                from.style.display = "none";
+                to.style.display = "block";
+            })
+            anim.play(); 
         },
         /**
          * Calls _showHelp with changelog (that's an included text module).
