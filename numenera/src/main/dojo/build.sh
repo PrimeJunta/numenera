70aed07aa9880a92934b8f2b5b0641149ef58767
psulonen <petteri.sulonen@iki.fi>
2013-10-25 22:46:04 +0300

    Improved docs and buildscript.

--- numenera/src/main/dojo/build.sh
+++ numenera/src/main/dojo/build.sh
@@ -1,6 +1,8 @@
 # Convenience script that copies resources from the typical git repository to
 # your current one, which should be the one containing dojo-src.
 #
+rm -rf ../release/*
+rm -rf primejunta
 cp -r ~/git/numenera/numenera/src/main/dojo/* .
 chmod u+x build.sh
 util/buildScripts/build.sh --profile numenera.profile.js
