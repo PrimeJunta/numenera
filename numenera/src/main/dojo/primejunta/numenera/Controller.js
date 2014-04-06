a3d4b60492ca3d0062173c55236bd86fe870186c
psulonen <petteri.sulonen@iki.fi>
2014-04-06 14:21:35 +0300

    Update to 3.2.0.

--- numenera/src/main/dojo/primejunta/numenera/Controller.js
+++ numenera/src/main/dojo/primejunta/numenera/Controller.js
@@ -17,7 +17,7 @@ function( declare,
 {
     return declare([ _ControllerBase ],
     {
-        version : "3.1.2",
+        version : "3.2.0",
         copyright : copyright,
         modules : {
             chargen : {
